51ec4af20df0ebccf09afa5386f44b1643458fd6
melkisedeath <manoskaristineos@gmail.com>
2021-09-22 17:17:10 +0200

    Fixed imports

--- src/models/rgcn-homo/entity_classify.py
+++ src/models/rgcn-homo/entity_classify.py
@@ -8,8 +8,6 @@ import argparse
 import numpy as np
 import time
 import os, sys
-from dgl import load_graphs
-from dgl.data.utils import load_info
 from models import *
 
 
@@ -22,37 +20,12 @@ from utils import *
 from entity_classify_mp import load_reddit
 
 
-def str_to_class(classname):
-    return getattr(sys.modules[__name__], classname)
-
-
 def normalize(x, eps=1e-8):
     normed = (x - x.mean(axis=0) ) / (x.std(axis=0) + eps)
     return normed
 
 
-def load_and_save(name, classname=None):
-    data_dir = os.path.abspath("./data/")
-    if os.path.exists(os.path.join(data_dir, name)):
-        # load processed data from directory `self.save_path`
-        graph_path = os.path.join(data_dir, name, name + '_graph.bin')
-        # Load the Homogeneous Graph
-        g = load_graphs(graph_path)[0][0]
-        info_path = os.path.join(data_dir, name, name + '_info.pkl')
-        n_classes = load_info(info_path)['num_classes']
-        return g, n_classes
-    else:
-        if classname:
-            dataset = str_to_class(classname)(save_path=data_dir)
-        else:
-            dataset = str_to_class(name)(save_path=data_dir)
-        
 
-        dataset.save_data()
-        # Load the Homogeneous Graph
-        g = dataset[0]
-        n_classes = dataset.num_classes 
-        return g, n_classes
 
 
 def main(args):
--- src/models/rgcn-homo/sweep-tune-hyperopt.py
+++ src/models/rgcn-homo/sweep-tune-hyperopt.py
@@ -21,6 +21,10 @@ if __name__ == '__main__':
                            help="Inductive learning setting")
     argparser.add_argument("--init-eweights", type=int, default=0, 
         help="Initialize learnable graph weights. Use 1 for True and 0 for false")
+    argparser.add_argument('--sample-gpu', action='store_true',
+                           help="Perform the sampling process on the GPU. Must have 0 workers.")
+    argparser.add_argument('--num-workers', type=int, default=4,
+                           help="Number of sampling processes. Use 0 for no extra process.")
     argparser.add_argument("--weight-decay", type=float, default=5e-4,
                         help="Weight for L2 loss")
     argparser.add_argument("--aggregator-type", type=str, default="pool",
@@ -30,6 +34,7 @@ if __name__ == '__main__':
                                 "on GPU when using it to save time for data copy. This may "
                                 "be undesired if they cannot fit in GPU memory at once. "
                                 "This flag disables that.")
+    argparser.add_argument('--fan-out', type=str, default='5, 10')
     # argparser.add_argument("--init_eweights", default=True, type=bool, help="Initialize edge weights")
     args = argparser.parse_args()
 
@@ -41,19 +46,24 @@ if __name__ == '__main__':
             dnum = "00"
         else:
             dnum = args.dataset[-2:]
-        config["wandb"] = {"project" : "Toy-"+dnum+"-Grid-Search"}
+        config["wandb"] = {"project" : "Toy-"+dnum+"-Grid-Search-MB"}
     else :
         raise ValueError("The Dataset is not Set for Optimization")
     config["lr"] = tune.grid_search([0.1, 0.01])
     config["num_hidden"] = tune.grid_search([8, 16, 32])
-    config["num_layers"] = tune.grid_search(list(range(6)))
+    config["num_layers"] = tune.grid_search([2])
     config["dropout"] = tune.grid_search([0.5])
     config["gnn"] = tune.grid_search(["SAGE"])
     config["init_eweights"] = tune.grid_search([0, 1])
 
     analysis = tune.run(
+        # your main function or script.py
         main,
-        loggers=[WandbLogger],  # WandbLogger logs experiment configurations and metrics reported via tune.report() to W&B Dashboard
+        # WandbLogger logs experiment configurations and metrics reported via tune.report() to W&B Dashboard
+        loggers=[WandbLogger],
+        # This resources per trial is a bit confusing to work with gpu nodes
+        # but usually just keeping it at 1 works in combination with : CUDA_AVAILABLE_DEVICES=0, 1, etc python scirpt.py.
         resources_per_trial={'gpu': args.num_gpu},
+        # Config is a dict with some tune.grid_Searchs or other tune hyparam opt.
         config=config)
 
