fe2b34d838630ac0b983dc14733f03515606ed83
melkisedeath <manoskaristineos@gmail.com>
2021-09-23 17:42:44 +0200

    Added dgl GAE model for edge creation/deletion

--- src/models/rgcn-homo/models.py
+++ src/models/rgcn-homo/models.py
@@ -133,7 +133,7 @@ class mylstmSAGEConv(nn.Module):
         # Aggregate
         h = self.aggregate(g, node_feats, e_weight)
         # Apply Learnable Weight
-        h = torch.squeeze(self.layer(h)[0])
+        h = th.squeeze(self.layer(h)[0])
         h = self.linear(h)
         # bias term
         h = h + self.bias
@@ -429,5 +429,76 @@ def expand_as_pair(input_, g=None):
 
 
 
+# ------------------------ GAE MODEL FOR LINK PREDICTIVE AUGMENTATION ----------------------------
 
-                    
+class NodeApplyModule(nn.Module):
+    def __init__(self, in_feats, out_feats, activation):
+        super(NodeApplyModule, self).__init__()
+        self.linear = nn.Linear(in_feats, out_feats)
+        self.activation = activation
+     
+    def forward(self, node):
+        h = self.linear(node.data['h'])
+        h = self.activation(h)
+        return {'h': h}
+
+
+
+class GCN(nn.Module):
+    def __init__(self, in_feats, out_feats, activation):
+        super(GCN, self).__init__()
+        self.apply_mod = NodeApplyModule(in_feats, out_feats, activation)
+        self.gcn_msg = fn.copy_src(src='h', out='m')
+        self.gcn_reduce = fn.sum(msg='m', out='h')  
+
+    def forward(self, g, feature):
+        # sum aggregation
+        g.ndata['h'] = feature
+        g.update_all(self.gcn_msg, self.gcn_reduce)
+        g.apply_nodes(func=self.apply_mod)
+        h =  g.ndata.pop('h')
+        return h
+
+
+class InnerProductDecoder(nn.Module):
+    def __init__(self, activation=th.sigmoid, dropout=0.1):
+        super(InnerProductDecoder, self).__init__()
+        self.dropout = dropout
+        self.activation = activation
+
+    def forward(self, z):
+        z = F.dropout(z, self.dropout)
+        adj = self.activation(th.mm(z, z.t()))
+        return adj
+
+
+class GAE(nn.Module):
+    def __init__(self, in_dim, hidden_dims):
+        super(GAE, self).__init__()
+        layers = [GCN(in_dim, hidden_dims[0], F.relu)]
+        if len(hidden_dims)>=2:
+            layers = [GCN(in_dim, hidden_dims[0], F.relu)]
+            for i in range(1,len(hidden_dims)):
+                if i != len(hidden_dims)-1:
+                    layers.append(GCN(hidden_dims[i-1], hidden_dims[i], F.relu))
+                else:
+                    layers.append(GCN(hidden_dims[i-1], hidden_dims[i], lambda x:x))
+        else:
+            layers = [GCN(in_dim, hidden_dims[0], lambda x:x)]
+        self.layers = nn.ModuleList(layers)
+        self.decoder = InnerProductDecoder(activation=lambda x:x)
+    
+    def forward(self, g):
+        h = g.ndata['h']
+        for conv in self.layers:
+            h = conv(g, h)
+        g.ndata['h'] = h
+        adj_rec = self.decoder(h)
+        return adj_rec
+
+    def encode(self, g):
+        h = g.ndata['h']
+        for conv in self.layers:
+            h = conv(g, h)
+        return h
+
