d2a708984361b4e2e0c6992b3c069423b656d403
melkisedeath <manoskaristineos@gmail.com>
2021-11-26 12:58:50 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+import setuptools
+
+
+
+setuptools.setup(
+    name='musym_GDL',
+    version='0.0.1dev',
+    description= 'Geometric Deep Learning on Symbolic Music Scores',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 0 - Gamma",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: MusicInformationRetrieval",
+    ],
+    author='Emmanouil Karystinaios',
+)
