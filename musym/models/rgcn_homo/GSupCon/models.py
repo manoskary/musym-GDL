1ea4fbf6c9908fd5e1672a6d5c5ff3e24e5e9b98
melkisedeath <manoskaristineos@gmail.com>
2022-01-25 15:49:40 +0100

    added Large Margin Cosine Loss for future use.

--- musym/models/rgcn_homo/GSupCon/models.py
+++ musym/models/rgcn_homo/GSupCon/models.py
@@ -259,6 +259,41 @@ class Encoder(nn.Module):
 		return adj
 
 
+class LMCL_loss(nn.Module):
+    """
+        Refer to paper:
+        Hao Wang, Yitong Wang, Zheng Zhou, Xing Ji, Dihong Gong, Jingchao Zhou,Zhifeng Li, and Wei Liu
+        CosFace: Large Margin Cosine Loss for Deep Face Recognition. CVPR2018
+        re-implement by yirong mao
+        2018 07/02
+    """
+
+    def __init__(self, num_classes, feat_dim, s=7.00, m=0.2):
+        super(LMCL_loss, self).__init__()
+        self.feat_dim = feat_dim
+        self.num_classes = num_classes
+        self.s = s
+        self.m = m
+        self.centers = nn.Parameter(torch.randn(num_classes, feat_dim))
+
+    def forward(self, feat, label):
+        batch_size = feat.shape[0]
+        norms = torch.norm(feat, p=2, dim=-1, keepdim=True)
+        nfeat = torch.div(feat, norms)
+
+        norms_c = torch.norm(self.centers, p=2, dim=-1, keepdim=True)
+        ncenters = torch.div(self.centers, norms_c)
+        logits = torch.matmul(nfeat, torch.transpose(ncenters, 0, 1))
+
+        y_onehot = torch.FloatTensor(batch_size, self.num_classes)
+        y_onehot.zero_()
+        y_onehot = torch.autograd.Variable(y_onehot).cuda()
+        y_onehot.scatter_(1, torch.unsqueeze(label, dim=-1), self.m)
+        margin_logits = self.s * (logits - y_onehot)
+
+        return logits, margin_logits
+
+
 class ContrastiveGraphSMOTE(nn.Module):
 	def __init__(self, in_feats, n_hidden, n_classes, n_layers, activation=F.relu, dropout=0.1):
 		super(ContrastiveGraphSMOTE, self).__init__()
