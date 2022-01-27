af0af4f6eadf351226485577fbb7f995ee3b3c4e
melkisedeath <manoskaristineos@gmail.com>
2022-01-27 14:40:10 +0100

    Adapted for multi-gpu training.

--- musym/models/rgcn_homo/GSupCon/CGSMOTE_sampling.py
+++ musym/models/rgcn_homo/GSupCon/CGSMOTE_sampling.py
@@ -1,18 +1,29 @@
 import os
-import time
 import torch
 import torch.nn as nn
 import dgl
-import numpy as np
 import tqdm
 from models import ContrastiveGraphSMOTE, SageClassifier
-from musym.models.rgcn_homo.GraphSMOTE.data_utils import load_imbalanced_local
+from musym.utils import load_and_save
 from pytorch_metric_learning.losses import SupConLoss
+import dgl.multiprocessing as mp
+from torch.nn.parallel import DistributedDataParallel
+import torchmetrics
 
-def main(args):
-    """
-    Main Call for Node Classification with Gaug.
-    """
+def run(proc_id, n_gpus, args, devices, data):
+    # Start up distributed training, if enabled.
+    dev_id = devices[proc_id]
+    if n_gpus > 1:
+        dist_init_method = 'tcp://{master_ip}:{master_port}'.format(
+            master_ip="140.78.124.137", # RK5
+            master_port='12345')
+        world_size = n_gpus
+        torch.distributed.init_process_group(backend="nccl",
+                                          init_method=dist_init_method,
+                                          world_size=world_size,
+                                          rank=proc_id)
+    torch.cuda.set_device(dev_id)
+
     #--------------- Standarize Configuration ---------------------
     config = args if isinstance(args, dict) else vars(args)
 
@@ -21,41 +32,21 @@ def main(args):
     config["log"] = False if config["unlog"] else True
 
     # --------------- Dataset Loading -------------------------
-    # g, n_classes = load_and_save("cad_basis_homo", config["data_dir"])
-    g, n_classes = load_imbalanced_local("BlogCatalog")
-
-    if "train_mask" in g.ndata.keys() and "val_mask" in g.ndata.keys() and "test_mask" in g.ndata.keys():
-        train_mask = g.ndata['train_mask']
-        val_mask = g.ndata['val_mask']
-        test_mask = g.ndata['test_mask']
-    else:
-        # --------------- Manually build train masks ------------------------
-        print("Using manual Masks ...")
-        train_mask = val_mask = test_mask = torch.zeros(g.num_nodes())
-
-        rand_inds = torch.tensor(range(g.num_nodes()))
-        np.random.shuffle(rand_inds)
-        train_inds = torch.tensor(rand_inds[:int(0.7*g.num_nodes())])
-        val_inds = torch.tensor(rand_inds[int(0.7 * g.num_nodes()) : int(0.8 * g.num_nodes())])
-        test_inds = torch.tensor(rand_inds[int(0.8*g.num_nodes()):])
-
-        train_mask[train_inds] = 1
-        val_mask[val_inds] = 1
-        test_mask[test_inds] = 1
+    g, n_classes = data
 
 
     # Hack to track node idx in dataloader's subgraphs.
-    labels = g.ndata['label']
+    labels = g.ndata.pop('label')
     train_nids = torch.nonzero(g.ndata.pop('train_mask'), as_tuple=True)[0]
-    node_features = g.ndata['feat']
+    node_features = g.ndata.pop('feat')
 
 
     # Validation and Testing
     val_nids = torch.nonzero(g.ndata.pop('val_mask'), as_tuple=True)[0]
     test_nids = torch.nonzero(g.ndata.pop('test_mask'), as_tuple=True)[0]
     # check cuda
-    use_cuda = config["gpu"] >= 0 and torch.cuda.is_available()
-    device = torch.device('cuda:%d' % torch.cuda.current_device() if use_cuda else 'cpu')
+    use_cuda = torch.cuda.is_available()
+    device = torch.device('cuda:%d' % dev_id if use_cuda else 'cpu')
     dataloader_device = "cpu"
     # create model
     in_feats = node_features.shape[1]
@@ -83,6 +74,7 @@ def main(args):
         device=dataloader_device,
         shuffle=config["shuffle"],
         batch_size = config["batch_size"],
+        use_ddp=True,
         drop_last=False,
         num_workers=config["num_workers"],
     )
@@ -90,8 +82,11 @@ def main(args):
     model = ContrastiveGraphSMOTE(in_feats, n_hidden=config["num_hidden"], n_classes=n_classes, n_layers=config["num_layers"])
     classifier = SageClassifier(in_feats=config["num_hidden"], n_hidden=config["num_hidden"], n_classes=n_classes, n_layers=1)
     if use_cuda:
-        model = model.cuda()
-        classifier = classifier.cuda()
+        model = model.to(dev_id)
+        classifier = classifier.to(dev_id)
+    if n_gpus > 1:
+        model = DistributedDataParallel(model, device_ids=[dev_id], output_device=dev_id)
+        classifier = DistributedDataParallel(classifier, device_ids=[dev_id], output_device=dev_id)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=config["lr"], weight_decay=config["weight_decay"])
     criterion = SupConLoss()
@@ -103,8 +98,8 @@ def main(args):
     for epoch in range(config["num_epochs"]):
         model.train()
         for step, (input_nodes, output_nodes, mfgs) in enumerate(tqdm.tqdm(train_dataloader, position=0, leave=True, desc="Pretraining Epoch %i" % epoch)):
-            batch_inputs = mfgs[0].srcdata['feat']
-            batch_labels = mfgs[-1].dstdata['label']
+            batch_inputs = node_features[input_nodes].to(device)
+            batch_labels = labels[output_nodes].to(device)
             adj = mfgs[-1].adj().to_dense()[:len(batch_labels), :len(batch_labels)].to(device)
             # Prediction of the Conctrastive model
             pred, upsampl_lab, embed_loss = model(mfgs, batch_inputs, adj, batch_labels)
@@ -112,29 +107,31 @@ def main(args):
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+        if n_gpus > 1:
+            torch.distributed.barrier()
 
+    model.eval()
+    classifier.train()
     for epoch in range(config["num_epochs"]):
-        model.eval()
-        classifier.train()
         for step, (input_nodes, output_nodes, mfgs) in enumerate(tqdm.tqdm(train_dataloader, position=0, leave=True, desc="Classifier Epoch %i" % epoch)):
-            batch_inputs = mfgs[0].srcdata['feat']
-            batch_labels = mfgs[-1].dstdata['label']
-            # Encoding of the Conctrastive model
-            batch_pred, prev_encs = model.encoder(mfgs, batch_inputs)
-            pred_adj = model.decoder(batch_pred, prev_encs)
-            if pred_adj.get_device() >= 0:
-                pred_adj = torch.where(pred_adj >= 0.5, pred_adj,
-                                       torch.tensor(0, dtype=pred_adj.dtype).to(batch_pred.get_device()))
-            else:
-                pred_adj = torch.where(pred_adj >= 0.5, pred_adj, torch.tensor(0, dtype=pred_adj.dtype))
+            with torch.no_grad():
+                batch_inputs = node_features[input_nodes].to(device)
+                batch_labels = labels[output_nodes].to(device)
+                # Encoding of the Contrastive model
+                batch_pred, prev_encs = model.module.encoder(mfgs, batch_inputs)
+                pred_adj = model.module.decoder(batch_pred, prev_encs)
             # Classifier Prediction
             batch_pred = classifier(pred_adj, batch_pred, prev_encs)
             acc = (torch.argmax(batch_pred, dim=1) == batch_labels).float().sum() / len(pred)
+            f1 = torchmetrics.functional.f1_score(torch.argmax(batch_pred, dim=1), batch_labels, average="macro")
             cl_loss = cl_criterion(batch_pred, batch_labels)
             cl_optimizer.zero_grad()
             cl_loss.backward()
             cl_optimizer.step()
-        print("Step {:04d} | Accuracy : {:.4f} | Loss : {:.4f} |".format(step, acc, loss))
+        if n_gpus > 1:
+            torch.distributed.barrier()
+        if dev_id == 0:
+            print("Step {:04d} | Accuracy : {:.4f} | F1 Macro {:.4f} | Loss : {:.4f} |".format(step, acc, f1, cl_loss))
 
     test_dataloader = dgl.dataloading.NodeDataLoader(
         g,
@@ -144,18 +141,20 @@ def main(args):
         shuffle=config["shuffle"],
         batch_size=config["batch_size"],
         drop_last=False,
+        use_ddp=True,
         num_workers=config["num_workers"],
     )
 
     acc = 0
+    classifier.eval()
     with torch.no_grad():
         for step, (input_nodes, output_nodes, mfgs) in enumerate(
                 tqdm.tqdm(test_dataloader, position=0, leave=True, desc="Epoch %i" % epoch)):
-            batch_inputs = mfgs[0].srcdata['feat']
-            batch_labels = mfgs[-1].dstdata['label']
+            batch_inputs = node_features[input_nodes].to(device)
+            batch_labels = labels[output_nodes].to(device)
             # Encoding of the Conctrastive model
-            batch_pred, prev_encs = model.encoder(mfgs, batch_inputs)
-            pred_adj = model.decoder(batch_pred, prev_encs)
+            batch_pred, prev_encs = model.module.encoder(mfgs, batch_inputs)
+            pred_adj = model.module.decoder(batch_pred, prev_encs)
             if pred_adj.get_device() >= 0:
                 pred_adj = torch.where(pred_adj >= 0.5, pred_adj,
                                        torch.tensor(0, dtype=pred_adj.dtype).to(batch_pred.get_device()))
@@ -164,6 +163,8 @@ def main(args):
             # Classifier Prediction
             batch_pred = classifier(pred_adj, batch_pred, prev_encs)
             acc += (torch.argmax(batch_pred, dim=1) == batch_labels).float().sum() / len(pred)
+        if n_gpus > 1:
+            torch.distributed.barrier()
     acc = acc/(step+1)
     print("Test accuracy: {:04f}".format(acc))
 
@@ -172,8 +173,8 @@ def main(args):
 if __name__ == '__main__':
     import argparse
     argparser = argparse.ArgumentParser(description='GraphSAGE')
-    argparser.add_argument('--gpu', type=int, default=0,
-                           help="GPU device ID. Use -1 for CPU training")
+    argparser.add_argument('--gpu', type=str, default='0',
+                           help="Comma separated list of GPU device IDs.")
     argparser.add_argument("-d", "--dataset", type=str, default="toy_01_homo")
     argparser.add_argument('--num-epochs', type=int, default=20)
     argparser.add_argument('--num-hidden', type=int, default=64)
@@ -193,12 +194,25 @@ if __name__ == '__main__':
                                 "This flag disables that.")
     argparser.add_argument("--init-eweights", type=int, default=0,
                            help="Initialize learnable graph weights. Use 1 for True and 0 for false")
-    argparser.add_argument("--temperature", type=float, default=0.2)
     argparser.add_argument("--data-dir", type=str, default=os.path.abspath("../data/"))
     argparser.add_argument("--unlog", action="store_true", help="Unbinds wandb.")
     args = argparser.parse_args()
-
-
-
     print(args)
-    main(args)
+    # --------------- Dataset Loading -------------------------
+    g, n_classes = load_and_save("cad_basis_homo", args.data_dir)
+    # g, n_classes = load_imbalanced_local("BlogCatalog")
+    g.create_formats_()
+    data = g, n_classes
+
+    devices = list(map(int, args.gpu.split(',')))
+    n_gpus = len(devices)
+    if n_gpus == 1:
+        run(0, n_gpus, args, devices, data)
+    else:
+        procs = []
+        for proc_id in range(n_gpus):
+            p = mp.Process(target=run, args=(proc_id, n_gpus, args, devices, data))
+            p.start()
+            procs.append(p)
+        for p in procs:
+            p.join()
